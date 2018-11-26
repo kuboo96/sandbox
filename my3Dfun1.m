6ecb548801701463c3e864f328c794581fe6b33f
Oliver Cizmazia <xcizmazia@stuba.sk>
2018-11-26 13:49:36 +0100

    my3Dfun1C

---
+++ my3Dfun1.m
@@ -0,0 +1,17 @@
+clear all
+clc
+close all
+
+x = -4:0.1:4;
+y = -2:0.1:2;
+[x,y] = meshgrid(x,y);
+Zc = x.*exp(-x.^2 - y.^2);
+figure
+surf(x,y,Zc)
+title('Z_c = x exp(-x^2 - y^2)')
+xlabel('x_c')
+ylabel('y_c')
+zlabel('Z_c')
+grid on
+colormap gray
+shading interp
